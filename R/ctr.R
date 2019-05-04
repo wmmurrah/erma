3e066f242919d46d7d43ac6b17d927d9281b15f0
wmmurrah <william.murrah@gmail.com>
2019-05-04 13:48:18 -0500

    added TODOs to ctr

--- R/ctr.R
+++ R/ctr.R
@@ -1,4 +1,6 @@
 # Center variable, defaulting to mean centering
+# TODO Add other character center values: min, max, median?
+# TODO Generalize for group mean centering.
 ctr <- function(x, constant = "mean", na.rm = FALSE) {
   cnst <- ifelse(constant == "mean", mean(x, na.rm = na.rm), constant)
   cx <- x - cnst
