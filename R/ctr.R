c36ebe97d645fff8d515a311e8fcfc3c59c89980
wmmurrah <william.murrah@gmail.com>
2020-12-03 19:48:31 -0600

    Remove DESCRIPTION

--- R/ctr.R
+++
@@ -1,8 +0,0 @@
-# Center variable, defaulting to mean centering
-# TODO Add other character center values: min, max, median?
-# TODO Generalize for group mean centering.
-ctr <- function(x, constant = "mean", ...) {
-  cnst <- ifelse(constant == "mean", mean(x), constant)
-  cx <- x - cnst
-  return(cx)
-}
